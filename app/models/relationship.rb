120ca9777c1336ff4a5b61cfc5ea73c6170222cd
Tobias Feistmantl <tobias@feistmantl.net>
2015-09-16 14:22:28 +0200

    Important patch which disallow user to follow himself

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,12 @@ class Relationship < ActiveRecord::Base
 	belongs_to :followed, class_name: "User", counter_cache: :follower_count
 
 	validates_presence_of [:follower, :followed]
+
+	validate :follower_and_followed_are_not_the_same
+
+	protected
+
+	def follower_and_followed_are_not_the_same
+		errors.add(:followed, "can't be the same as the follower") if followed == follower
+	end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Relationship, type: :model do
 
 	it { is_expected.to validate_presence_of :follower }
 	it { is_expected.to validate_presence_of :followed }
+
+	describe "same value for follower and followed" do
+		let(:user) { create(:user) }
+		subject { build(:relationship, follower: user, followed: user) }
+
+		it { is_expected.to_not be_valid }
+
+		it "returns an error" do
+			subject.valid?
+			expect(subject.errors.messages[:followed]).to include("can't be the same as the follower")
+		end
+
+		it "does not save the subject" do
+			expect(subject.save).to be_falsey
+		end
+	end
 end
