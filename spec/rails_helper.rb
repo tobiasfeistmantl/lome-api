ee5919a1a4bd7205c4e14b266142cecc1afa291f
Tobias Feistmantl <tobias@feistmantl.net>
2015-09-30 13:36:45 +0200

    Update DatabaseCleaner config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,14 +37,19 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
+    DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
   end
 
   config.after(:each) do
